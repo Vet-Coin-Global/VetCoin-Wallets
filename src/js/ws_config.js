6f456e29d532e6e378b1e3b2bc0e106ba4cd7c3d
blockinator <36169687+blockinator@users.noreply.github.com>
2021-02-11 10:31:34 -0800

    Initial Upload

---
+++ src/js/ws_config.js
@@ -0,0 +1,97 @@
+var config = {};
+
+// self explanatory, your application name, descriptions, etc
+config.appName = 'VetCoin';
+config.appDescription = 'VetCoin Wallet';
+config.appSlogan = 'Fast and private!';
+config.appId = 'com.vetcoin2020.walletshell';
+config.appGitRepo = 'https://github.com/Vet-Coin-Global/VetCoin-Wallets';
+
+// default port number for your daemon (e.g. TurtleCoind)
+config.daemonDefaultRpcPort = 11898;
+
+// wallet file created by this app will have this extension
+config.walletFileDefaultExt = 'wallet';
+
+// change this to match your wallet service executable filename
+config.walletServiceBinaryFilename = 'vetcoin-service';
+
+// version on the bundled service (vetcoin-service)
+config.walletServiceBinaryVersion = "v0.13.0";
+
+// config file format supported by wallet service, possible values:
+// ini -->  for turtle service (or its forks) version <= v0.8.3
+// json --> for turtle service (or its forks) version >= v0.8.4
+config.walletServiceConfigFormat = "json";
+
+// default port number for your wallet service (e.g. turtle-service)
+config.walletServiceRpcPort = 8070;
+
+// block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
+config.blockExplorerUrl = 'http://207.244.228.208/?hash=[[TX_HASH]]#blockchain_transaction';
+
+// default remote node to connect to, set this to a known reliable node for 'just works' user experience
+config.remoteNodeDefaultHost = '161.97.172.201';
+
+
+// remote node list update url, set to null if you don't have one
+// for TRTL:
+// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
+// filtered: https://trtl.nodes.pub/api/getNodes
+config.remoteNodeListUpdateUrl = 'https://raw.githubusercontent.com/Vet-Coin-Global/VetCoin-nodes-json/master/vetcoin-nodes.json';
+
+// set to false if using raw/unfiltered node list
+config.remoteNodeListFiltered = false;
+
+// fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
+config.remoteNodeListFallback = [
+  '207.244.228.208:11898',
+  '161.97.172.201:11898'
+];
+config.remoteNodeCacheSupported = false;
+config.remoteNodeSslSupported = false;
+
+// your currency name
+config.assetName = 'VetCoin';
+// your currency ticker
+config.assetTicker = 'VET';
+// your currency address prefix, for address validation
+config.addressPrefix = 'VC';
+// standard wallet address length, for address validation
+config.addressLength = 98;
+// integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
+config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+
+// minimum fee for sending transaction
+config.minimumFee = 0.0001;
+// minimum amount for sending transaction
+config.mininumSend = 0.1;
+// default mixin/anonimity for transaction
+config.defaultMixin = 0;
+// to represent human readable value
+config.decimalPlaces = 5;
+// to convert from atomic unit
+config.decimalDivisor = 10 ** config.decimalPlaces;
+
+// obfuscate address book entries, set to false if you want to save it in plain json file.
+// not for security because the encryption key is attached here
+config.addressBookObfuscateEntries = true;
+// key use to obfuscate address book contents
+config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6fba5';
+// initial/sample entries to fill new address book
+config.addressBookSampleEntries = [
+  {
+    name: 'Vet Coin Donation',
+    address: 'VCzHTumECaaEmJ6ikKV87jHQ9JDPsCoQCh3c5ZZFC84v6kN4vzDZAmkaVQShjjTTV3JC93cdcpvf6eknADrxdZhy5Z327eiYVj',
+    paymentId: '',
+  }
+];
+// cipher config for private address book
+config.addressBookCipherConfig = {
+  algorithm: 'aes-256-gcm',
+  saltLenght: 128,
+  pbkdf2Rounds: 10000,
+  pbkdf2Digest: 'sha512'
+};
+
+module.exports = config;
